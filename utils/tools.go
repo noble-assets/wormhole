8049728512c3a0987a16de4f798fb12c4ec1d391
John Letey <john@noble.xyz>
2025-02-04 14:07:27 +0100

    chore: add space back

--- utils/tools.go
+++ utils/tools.go
@@ -22,7 +22,7 @@
 
 // This file uses the recommended method for tracking developer tools in a Go module.
 //
-// REF:https://go.dev/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+// REF: https://go.dev/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 package tools
 
 import (
